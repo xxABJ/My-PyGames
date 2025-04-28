328bb34efa61469c65e64aa67a0e68744ec9f839
Abj <silv3r-qtr@hotmail.com>
2025-04-28 23:23:00 +0300

    Chnages:
    || snakes_and_ladders/classing/
    - Added a new directory to test out classing the whole game!
    - Added a new python file: snakes_and_ladders/classing/game.py
    - Added a new python file: snakes_and_ladders/classing/main.py
    - Added a new python file: snakes_and_ladders/classing/menu.py
    
    || snakes_and_ladders/classing/game.py
    - Created a class Game(), containing an initializer with main variables for initializing a pygame window and a display surface.
    - Added a function check_events(self), to check for pygame events that match boolean interaction keys specified in the main initializer.
    - Added a function reset_keys(self), to reset the boolean variables in the initializer function.
    - Added a function draw_text(self, text, size, x, y), to take in a string, a size and coordinates, which then creates a text surface using pygame statements,
      then uses a built-in pygame function that gets the dimentions of said string, assignes a center point on the surface, and finally blits it on the display
      surface variable found in the main initializer function.
    - Added a function game_loop(self), to start a loop depending on a boolean variable in the main initializer called self.playing, which calls the check_events(self)
      function, then changes the dependent loop variable if a specific key is triggered (checks it's boolean value), then resets the display surface, then calls the
      draw_text(self, text, size, x, y) function to blit a string to the dispaly surface, then blits the display surface to the main pygame window, then updates the
      display using a built-in pygame statement and finally calls the reset_keys(self) function to reset the boolean values (specific keys) assigned in the main initializer.
    
    To do:
    - Continue watched the youtube videos to start on the different main states for the game.
    - Try to implement classes to the main game!

---
+++ snakes_and_ladders/classing/game.py
@@ -0,0 +1,60 @@
+import pygame
+
+# gameclass - https://www.youtube.com/@CDcodes
+
+# make a menu class that generates a menu on the pygame window.
+# make a button class that generates buttons on the menu/other.
+# make a game class? (prob have to draw the actual game.. maybe)
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.running, self.playing = True, False
+        self.ESCAPE_KEY, self.ENTER_KEY, self.UP_KEY, self.DOWN_KEY, self.LEFT_KEY, self.RIGHT_KEY, self.BACKSPACE_KEY = False, False, False, False, False, False, False
+        self.DISPLAY_W, self.DISPLAY_H = 800, 600
+        self.display_surface = pygame.Surface((self.DISPLAY_W, self.DISPLAY_H))
+        self.window = pygame.display.set_mode((self.DISPLAY_W, self.DISPLAY_H))
+        self.font_name = pygame.font.get_default_font()
+        self.HIGHLIGHTED, self.MENUCOLOUR, self.WHITE = (120, 170, 60), (80, 110, 100), (255, 255, 255)
+    
+    def check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running, self.playing = False, False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.ESCAPE_KEY = True
+                if event.key == pygame.K_RETURN:
+                    self.ENTER_KEY = True
+                if event.key == pygame.K_UP:
+                    self.UP_KEY = True
+                if event.key == pygame.K_DOWN:
+                    self.DOWN_KEY = True
+                if event.key == pygame.K_LEFT:
+                    self.LEFT_KEY = True
+                if event.key == pygame.K_RIGHT:
+                    self.RIGHT_KEY = True
+                if event.key == pygame.K_BACKSPACE:
+                    self.BACKSPACE_KEY = True
+    
+    def reset_keys(self):
+        self.ESCAPE_KEY, self.ENTER_KEY, self.UP_KEY, self.DOWN_KEY, self.LEFT_KEY, self.RIGHT_KEY, self.BACKSPACE_KEY = False, False, False, False, False, False, False
+
+    def game_loop(self):
+        while self.playing:
+            self.check_events()
+            if self.ESCAPE_KEY:
+                self.playing = False
+            self.display_surface.fill(self.MENUCOLOUR)
+            self.draw_text("We are inside the game!", 30, self.DISPLAY_W/2, self.DISPLAY_H/2) # between display resetting & main window blitting
+            self.window.blit(self.display_surface, (0, 0))
+            pygame.display.update()
+            self.reset_keys()
+
+    def draw_text(self, text, size, x, y):
+        font = pygame.font.Font(self.font_name, size)
+        text_surface = font.render(text, True, self.WHITE)
+        text_rect = text_surface.get_rect()
+        text_rect.center = (x, y)
+        self.display_surface.blit(text_surface, text_rect)
+    
